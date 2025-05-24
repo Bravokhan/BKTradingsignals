0a187802b007dc26ed211e5681a41dc669117013
Bravokhan <asifnadan2016@gmail.com>
2025-05-24 21:06:37 +0500

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,10 +6,6 @@ import pandas as pd
 import pandas_ta as ta
 from streamlit_autorefresh import st_autorefresh
 
-
-# Auto-refresh every 60 seconds
-st_autorefresh(interval=60 * 1000, key="refresh")
-
 st.markdown("""<h1 style='text-align: center; color: #00ffcc;'>📈 PX Signal Bot - Live Trading Signals</h1>""", unsafe_allow_html=True)
 
 # Display local time (UTC+5)
